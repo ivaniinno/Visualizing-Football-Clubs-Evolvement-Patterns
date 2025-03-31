4908fafc7bea34925368b7cd000f4a62d5a8dc28
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-03-31 15:08:17 +0300

    chore: remove CSP headers temporarily

--- app/app.py
+++ app/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, send_from_directory, make_response
 from routes.api import api_bp
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(api_bp)
+swagger = Swagger(app)
 
 csp = (
-        f"default-src *;"
-        f"script-src 'self'"
-    )
+    "default-src 'self';"
+    "script-src 'self' https://cdn.jsdelivr.net;"
+    "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline';"
+    "img-src 'self' data: https://*.swagger.io;"
+)
 
-@app.after_request
-def apply_csp_headers(resp):
-    resp.headers['Content-Security-Policy'] = csp
-    return resp
+# @app.after_request
+# def apply_csp_headers(resp):
+#     resp.headers['Content-Security-Policy'] = csp
+#     return resp
 
 
 @app.route('/')
