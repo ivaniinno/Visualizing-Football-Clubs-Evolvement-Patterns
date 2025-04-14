ff175ab1c60c0d77e1cb830db9922e41c3d83b38
Maksim Ilin <en3rgy.channel4@gmail.com>
2025-04-14 12:28:12 +0300

    chore: delete unnecessary files and functions

--- app/app.py
+++ app/app.py
@@ -1,24 +1,7 @@
-from flask import Flask, render_template, send_from_directory, make_response
+from flask import Flask
 from routes.api import api_bp
 from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(api_bp)
 swagger = Swagger(app)
-
-csp = (
-    "default-src 'self';"
-    "script-src 'self' https://cdn.jsdelivr.net;"
-    "style-src 'self' https://cdn.jsdelivr.net 'unsafe-inline';"
-    "img-src 'self' data: https://*.swagger.io;"
-)
-
-# @app.after_request
-# def apply_csp_headers(resp):
-#     resp.headers['Content-Security-Policy'] = csp
-#     return resp
-
-
-@app.route('/')
-def index():
-    return make_response(render_template('index.html'))
